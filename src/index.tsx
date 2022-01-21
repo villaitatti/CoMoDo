4fb87f795d93f506378b0dc29b72642a58817d21
gspinaci <gianmarco.spinaci@gmail.com>
2022-01-21 17:52:49 +0100

    init canvas
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom';
+import React from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import createEngine, { 
+  DefaultLinkModel, 
+  DefaultNodeModel,
+  DiagramModel 
+} from '@projectstorm/react-diagrams';
+
+import {
+  CanvasWidget
+} from '@projectstorm/react-canvas-core';
+
+// create an instance of the engine with all the defaults
+const engine = createEngine();
+
+// node 1
+const node1 = new DefaultNodeModel({
+  name: 'E22_Man-Made_Object',
+  color: 'rgb(0,192,255)',
+});
+node1.setPosition(100, 100);
+let port1 = node1.addOutPort('');
+
+// node 2
+const node2 = new DefaultNodeModel({
+  name: 'E12_Production',
+  color: 'rgb(0,192,255)',
+});
+node2.setPosition(400, 200);
+let port2 = node2.addOutPort('');
+
+// link them and add a label to the link
+const link = port1.link<DefaultLinkModel>(port2);
+link.addLabel('crm:P108i_was_produced_by');
+
+const model = new DiagramModel();
+model.addAll(node1, node2, link);
+engine.setModel(model);
+
+const canvas = <CanvasWidget className="diagram-container" engine={engine} />;
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    canvas,
+  document.getElementById('application')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
